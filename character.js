bacf9bd3891639e91ecdc633e3d3d850757783c0
Ghosticface <maldeathenize@gmail.com>
2023-12-07 18:21:20 +0100

    tessssttttt

---
+++ character.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let textbox = document.getElementById("textbox");
+
+    // Define texts for different elements
+    const texts = {
+        Veri: "Veroni",
+        Hilda: "Hilda",
+        Valerian: "Valerian",
+        Huhner: "HUH?",
+        Beatrice: "Beatrice",
+        Veri: "Veroni",
+        Veri: "Veroni",
+        Veri: "Veroni",
+        Veri: "Veroni",
+        Diph: "Dip",
+        NoChar: "???",
+    };
+
+    // Add event listeners for all elements
+    document.querySelectorAll('[id]').forEach((element) => {
+        element.addEventListener("mouseenter", (event) => {
+            const id = element.id;
+            textbox.innerHTML = texts[id] || "";
+        });
+
+        element.addEventListener("mouseleave", (event) => {
+            textbox.innerHTML = "";
+        });
+    });
+});
